67e708589eb090b33acd5cfbfa4aa556976c882c
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2025-04-27 08:04:02 -0500

    🔨 [ADD] class abstracscreen

---
+++ core/src/main/java/io/game/screens/AbstractScreen.java
@@ -0,0 +1,47 @@
+package io.game.screens;
+
+import com.badlogic.gdx.Screen;
+import io.game.MainGame;
+
+public class AbstractScreen implements Screen {
+    protected final MainGame game;
+
+    public AbstractScreen(MainGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
