25322b66f4cbd7a86ecb761a9d3062c8fb44584c
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2025-04-28 14:43:47 -0500

    🔧 [CHANGE] parameter collision 20 to 30

--- core/src/main/java/io/game/gamesobject/CollisionManager.java
+++ core/src/main/java/io/game/gamesobject/CollisionManager.java
@@ -7,7 +7,7 @@ public class CollisionManager {
     public static boolean isCollision(Bullet bullet, Pj pj){
         float dx = bullet.getPosition().x - pj.getPosition().x;
         float dy = bullet.getPosition().y - pj.getPosition().y;
-        return Math.sqrt(dx * dx + dy * dy) < 20;
+        return Math.sqrt(dx * dx + dy * dy) < 30;
     }
 
 }
