7568632b27b20cb821338132217cbb871af8336b
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2025-04-30 09:08:53 -0500

    🔨 [ADD] method setFond and agregued owenerId

--- core/src/main/java/io/game/gamesobject/Bullet.java
+++ core/src/main/java/io/game/gamesobject/Bullet.java
@@ -9,12 +9,37 @@ public class Bullet {
     private float speed; // Velocidad vertical (positiva para nave, negativa para enemigos)
     private String design;
     private BitmapFont font;
+    private int ownerId;
 
-    public Bullet(float x, float y, float speed, String design, BitmapFont font) {
+    public Bullet(float x, float y, float speed, String design,int ownerId) {
         this.position = new Vector2(x, y);
         this.speed = speed;
         this.design = design;
-        this.font = font;
+        this.ownerId = ownerId;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public String getDesign() {
+        return design;
+    }
+
+    public void setDesign(String design) {
+        this.design = design;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
     }
 
     public void update(float delta) {
@@ -31,5 +56,13 @@ public class Bullet {
 
     public boolean isOutOfScreen() {
         return position.y > 600 || position.y < 0;
+    }
+
+    public BitmapFont getFont() {
+        return font;
+    }
+
+    public void setFont(BitmapFont font) {
+        this.font = font;
     }
 }
